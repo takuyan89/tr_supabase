b404c3a781703c0af35d0213eaa38a3e1c3a4d12
Takuya Nomura <git.takuya89@gmail.com>
2024-11-19 02:30:57 +0100

    useform

--- src/app/task/new/page.tsx
+++ src/app/task/new/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SubmitHandler, useForm } from "react-hook-form";
 type Task = {
   name: string;
@@ -9,21 +11,29 @@ const CrateTaskPage = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Task>();
+  } = useForm<Task>({ defaultValues: { name: "walking" } });
+
+  console.log("errors: ", errors);
+
+  const name = watch("name");
 
   const onSubmit: SubmitHandler<Task> = (data) => console.log(data);
 
   return (
-    <div>
-      <h1>Task</h1>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-xl">Task</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <span>Task名</span>
+        <label htmlFor="name">TaskName: </label>
         <input
+          className="border border-gray-500 rounded"
           type="text"
+          id="name"
           {...register("name", {
             required: "Task名を入力してください",
           })}
         />
+        <input type="submit" className="border border-black ml-2 rounded" />
+        <p>{name}</p>
       </form>
     </div>
   );
